9891f836bb68368c84b72e1c997991dfb1991c0f
ImpactaDenis <denis.tiago@aluno.faculdadeimpacta.com.br>
2023-05-20 18:31:32 -0300

    first commit

---
+++ interact_s3.py
@@ -0,0 +1,16 @@
+import boto3
+import pandas as pd
+
+# Criar um cliente para interagir com o AWS S3
+s3_client = boto3.client('s3')
+
+s3_client.download_file("datalake-ney-igti",
+                        "data/Observabilidade.txt",
+                        "data/Observabilidade.txt")
+
+df = pd.read_csv("data/Observabilidade.txt", sep=";")
+print(df)
+
+# s3_client.upload_file("data/pnadc20203.csv",
+#                     "datalake-ney-igti-edc",
+#                     "data/pnadc20203.csv")
