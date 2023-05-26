d54cd0aed2fdf1453181d4e067abf1b40aee0db5
ImpactaDenis <denis.tiago@aluno.faculdadeimpacta.com.br>
2023-05-25 23:50:31 -0300

    add arquivos tf, codigos py e shell para lambda.

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -5,7 +5,7 @@ provider "aws" {
 # Centralizar o arquivo de controle de estado do terraform
 terraform {
   backend "s3" {
-    bucket = "terraform-state-igti-ney-2"
+    bucket = "terraform-state-igti-denis"
     key = "state/igti/edc/mod1/terraform.tfstate"
     region = "us-east-2"
   }
--- interact_s3.py
+++ interact_s3.py
@@ -4,13 +4,14 @@ import pandas as pd
 # Criar um cliente para interagir com o AWS S3
 s3_client = boto3.client('s3')
 
-s3_client.download_file("datalake-ney-igti",
-                        "data/Observabilidade.txt",
-                        "data/Observabilidade.txt")
+# s3_client.download_file("datalake-ney-igti",
+#                         "data/Observabilidade.txt",
+#                         "data/Observabilidade.txt")
 
-df = pd.read_csv("data/Observabilidade.txt", sep=";")
-print(df)
+# df = pd.read_csv("data/Observabilidade.txt", sep=";")
+# print(df)
 
-# s3_client.upload_file("data/pnadc20203.csv",
-#                     "datalake-ney-igti-edc",
-#                     "data/pnadc20203.csv")
+s3_client.upload_file("data/pnadc2020.csv",
+                    "datalake-denissilva-589255375735",
+                    "raw-data/pnadc2020.csv")
+# s3_client.Bucket('datalake-denissilva-589255375735').upload_file('data/pnadc2020.csv', 'raw-data/pnadc2020.csv')
